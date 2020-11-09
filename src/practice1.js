443d81a83d64f0d0bcc3287f29e7e54aa9eaf9bc
Yanqin Shen <yanqin.shen@thoughtworks.com>
2020-11-09 23:38:59 +0800

    [syq] feat: complete all

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = (input) => {
+  const { data, column } = input
+  const keys = column.map(item => item.name)
+  const output = data.map((item) => {
+    const obj = {}
+    keys.map((name, index) => {
+      obj[name] = item[index]
+    })
+    return obj
+  })
+  return output
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items, sections) => {
+  let output  = [...items]
+  sections.forEach((section, index) => {
+    output.splice(section.index + index, 0, section.content)
+  })
+  return output
+}
 export { inject };
